7492f07f2c5073605e38bb4e1f771d797a74f165
lmas <lmasvensson@gmail.com>
2013-09-14 17:12:28 +0200

    added hosts.sh

---
+++ hosts.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Create a new hosts file and fill it with blocked adservers.
+
+FILE='hosts'
+
+echo "# HOSTS file based on http://www.mvps.org/winhelp2002/hosts.txt" > $FILE
+echo "" >> $FILE
+echo "#<ip-address>   <hostname.domain.org>   <hostname>" >> $FILE
+echo "127.0.0.1       localhost" >> $FILE
+echo "::1             localhost" >> $FILE
+echo "192.168.1.1     gateway $(hostname)" >> $FILE
+echo "" >> $FILE
+
+wget -q -O - http://www.mvps.org/winhelp2002/hosts.txt | \
+grep -v localhost | \
+grep 127.0.0.1 | \
+awk '{print "0.0.0.0 " $2}' >> $FILE
+
